83a95455ac866116e45b89667ab7b91306a12d23
snack <kwansiknam@gmail.com>
2023-08-23 00:19:55 +0900

    Initial Commit

---
+++ NC-ChatGPT/NC_ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  NC_ChatGPTApp.swift
+//  NC-ChatGPT
+//
+//  Created by 관식 on 2023/08/23.
+//
+
+import SwiftUI
+
+@main
+struct NC_ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
